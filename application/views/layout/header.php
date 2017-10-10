e2dfd6f697a8b3f5439dfb53766f26fd648920e9
minhtuan9922 <minhtuan9922@gmail.com>
2017-10-10 19:49:00 +0700

    tuan cap nhat

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -17,12 +17,11 @@
              	<a href="index.php"><img src="images/dangtin.png"/>ĐĂNG TIN MIỄN PHÍ </a>
              </div>
              <div class="dangnhap" >
-             	<a href="index.php"><img src="images/login.png"/>Đăng Nhập </a>
+             	<a href="#" data-toggle="modal" data-target="#dangnhap"><img src="images/login.png"/>Đăng Nhập </a>
              </div>
              <div class="dangky" >
              	<a href="index.php"><img src="images/add_user.png"/>Đăng ký </a>
-             </div>
-        
+             </div>        
 		<div class="head-nav">
 			<div class="container">
 				<span class="menu">Menu</span>
@@ -39,6 +38,44 @@
                         -->
 							<div class="clearfix"> </div>
 					</ul>
+			</div>
+		</div>
+		<!-- Modal đăng nhập-->
+		<div class="modal fade" id="dangnhap" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+						<h4 class="modal-title" id="myModalLabel">Modal title</h4>
+					</div>
+					<div class="modal-body">
+						<form class="form-horizontal" role="form">
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+								<div class="col-sm-10">
+									<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+								<div class="col-sm-10">
+									<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-offset-2 col-sm-10">
+									<div class="checkbox">
+										<label><input type="checkbox" checked> Remember me </label>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Save changes</button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!-- script-for-nav -->
