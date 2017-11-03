faa18f24980d07a2853afb35d9d2d079b5add47c
minhtuan9922 <minhtuan9922@gmail.com>
2017-11-03 23:45:54 +0700

    them trang admin

---
+++ application/controllers/admin/home.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->load->model('viec_lam');
+		$this->load->model('nguoi_tim_viec');
+	}
+	public function index()
+	{
+		$data['title'] = 'Trang quản trị';
+		$data['content'] = 'admin/home';
+		$data['active'] = 1;
+		$data['vieclam'] = $this->viec_lam->vieclam();
+		$data['nguoitimviec'] = $this->nguoi_tim_viec->vieclam();
+		$this->load->view('admin/layout', $data);
+		
+	}	
+	public function dangnhap()
+	{
+		$this->load->model('nguoi_tim_viec');
+		//$this->load->model('nha_tuyen_dung');
+		if(isset($_POST['dangnhap']))
+		{
+			$tuychon = $this->input->post('tuychon');
+			$email = $this->input->post('email');
+			$pass = md5($this->input->post('pass'));
+			if($tuychon == 1)
+			{
+				$check = $this->nguoi_tim_viec->dangnhap($email,$pass);
+				if($check == TRUE)
+				{
+					$this->session->set_userdata("login", $email);
+					//echo $_SESSION['login'];
+					redirect(base_url());
+				}
+			}
+		}
+	}
+	public function dangxuat()
+	{
+		//đăng xuất
+		$this->session->unset_userdata("login");
+		redirect(base_url());
+		//view
+		
+	}
+}
