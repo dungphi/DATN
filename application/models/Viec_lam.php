9baf0f23ae601be37c2922faa47aee9d509d02f5
minhtuan9922 <minhtuan9922@gmail.com>
2017-11-23 09:37:26 +0700

    cap nhat admin

--- application/models/Viec_lam.php
+++ application/models/Viec_lam.php
@@ -10,22 +10,7 @@ class Viec_lam extends CI_Model{
 	public function vieclam()
 	{
 		$this->db->select('*');
-<<<<<<< HEAD
-		$this->db->from('viec_lam,nha_tuyen_dung,nganh_nghe,dia_diem');
-		$this->db->where('viec_lam.id_ntd = nha_tuyen_dung.id');
-		$this->db->where('viec_lam.nganh = nganh_nghe.id_nn');
-		$this->db->where('viec_lam.vi_tri = dia_diem.id_dd');
-		$this->db->order_by('nha_tuyen_dung.id', 'desc'); //asc
-		//$this->db->limit('6');
-		return $this->db->get()->result_array();
-	}
-	public function capnhat_vl($data=array(),$id)
-	{
-		$this->db->where('id_vl',$id);
-        return $this->db->update('viec_lam',$data);
-	}
 
-=======
 		$this->db->from('nha_tuyen_dung,viec_lam,nganh_nghe,dia_diem,kinh_nghiem,muc_luong,gioi_tinh,trinh_do');
 		$this->db->where('nha_tuyen_dung.id_ntd = viec_lam.id_ntd');
 		$this->db->where('viec_lam.id_nn = nganh_nghe.id_nn'); 
@@ -38,6 +23,13 @@ class Viec_lam extends CI_Model{
 		//$this->db->limit('6');
 		return $this->db->get()->result_array();
 	}
+	public function capnhat_vl($data=array(),$id)
+	{
+		$this->db->where('id_vl',$id);
+        return $this->db->update('viec_lam',$data);
+	}
+
+		
 	public function vieclamchitiet($id)
 	{
 		$this->db->select('*');
@@ -63,7 +55,6 @@ class Viec_lam extends CI_Model{
 //		return $this->db->get()->row_array();
 //	}
 //
->>>>>>> 01fc3629827a60029113f6dd13ab660ab55f1f5a
 //	public function countAll(){
 //		return $this->db->count_all($this->_table); 
 //	}
